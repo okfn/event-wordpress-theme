f1c5719a6fa05491c6477d24eed1c599ddfcaf35
Sam Smith <sam@mintcanary.com>
2013-03-20 13:57:11 +0000

    moved sharing

--- page-homepage.php
+++ page-homepage.php
@@ -136,20 +136,17 @@ Template Name: Homepage
 						</section> <!-- end article header -->
 						
 						<footer>
-			
 							<p class="clearfix"><?php the_tags('<span class="tags">' . __("Tags","bonestheme") . ': ', ', ', '</span>'); ?></p>
-							
+							<?php if(of_get_option('site_sharing', '1') && of_get_option('home_sharing')) {?>
+              <ul class="socialcount" data-url="<?php echo get_permalink( $post->ID ); ?>">
+                <li class="facebook"><a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink( $post->ID ); ?>" title="Share on Facebook"><span class="icon icon-facebook"></span><span class="count">Like</span></a></li>
+                <li class="twitter"><a href="https://twitter.com/intent/tweet?text=<?php echo get_permalink( $post->ID ); ?>" title="Share on Twitter"><span class="icon icon-twitter"></span><span class="count">Tweet</span></a></li>
+                <li class="googleplus"><a href="https://plus.google.com/share?url=<?php echo get_permalink( $post->ID ); ?>" title="Share on Google Plus"><span class="icon icon-googleplus"></span><span class="count">+1</span></a></li>        
+              </ul>
+              <?php } ?>
 						</footer> <!-- end article footer -->
 					
 					</article> <!-- end article -->
-					
-          <?php if(of_get_option('site_sharing', '1') && of_get_option('home_sharing')) {?>
-          <ul class="socialcount" data-url="<?php echo get_permalink( $post->ID ); ?>">
-            <li class="facebook"><a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_permalink( $post->ID ); ?>" title="Share on Facebook"><span class="icon icon-facebook"></span><span class="count">Like</span></a></li>
-            <li class="twitter"><a href="https://twitter.com/intent/tweet?text=<?php echo get_permalink( $post->ID ); ?>" title="Share on Twitter"><span class="icon icon-twitter"></span><span class="count">Tweet</span></a></li>
-            <li class="googleplus"><a href="https://plus.google.com/share?url=<?php echo get_permalink( $post->ID ); ?>" title="Share on Google Plus"><span class="icon icon-googleplus"></span><span class="count">+1</span></a></li>        
-          </ul>
-          <?php } ?>
           
 					<?php 
 						// No comments on homepage
