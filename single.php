89e69d6b7a9f40ff61829417876a8cb8f543fb62
Sam Smith <sam@mintcanary.com>
2013-07-05 17:15:55 +0100

    move while

--- single.php
+++ single.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			<div class="page-header hero-unit">
         <h1 class="single-title" itemprop="headline"><?php the_title(); ?></h1>
         <p class="meta"><?php _e("Posted", "bonestheme"); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_date(); ?></time> <?php _e("by", "bonestheme"); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php _e("filed under", "bonestheme"); ?> <?php the_category(', '); ?>.</p>
@@ -8,7 +9,6 @@
 			
 				<div id="main" class="span8 clearfix" role="main">
 
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					
 					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 						
